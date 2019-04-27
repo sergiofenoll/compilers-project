52a6701dd54246cbe3fcf4b12c175f94f7036193
MeyerJon <Jonathan.Meyer@student.uantwerpen.be>
2019-04-27 16:12:17 +0200

    Fixed bug in creating llvm ir file

--- example-C-code/simple.c
+++ example-C-code/simple.c
@@ -1,11 +1,4 @@
-#include <stdio.h>
-
 int main() {
     int a = 5;
-//    int *ptr_to_a = &a;
-//    int b = *ptr_to_a;
-    int b = a;
-    printf("%d\n", a);
-    printf("%d\n", b);
     return 0;
 }
